e0a806773b491e573dac47320296b68377ece03b
Sadashiv H <hsadashiv@zeomega.com>
2012-05-28 11:38:00 +0530

    The shell script is installing python in the current directory with the /usr/bin/python

--- installpy.sh
+++ installpy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-PREFIX='pwd'/usr
-cd downloads
+PREFIX=`pwd`/usr
+#cd downloads
 wget -c http://www.python.org/ftp/python/2.6.8/Python-2.6.8.tar.bz2
 tar jxvf Python-2.6.8.tar.bz2
-echo `pwd`
 mkdir $PREFIX
 cd Python-2.6.8
-./configure --prefix= --enable-unicode=ucs4 
+./configure --prefix=$PREFIX 
 make 
 make install
-echo `pwd`
-cd ../
-#rm -fr Python-2.6.8
+rm -fr Python-2.6.8
 echo "Python installation done"
