8b2b4935bb7a26a384d7b1f5ec93e83006e33f35
Sadashiv Hanamant <sadamsrit@gmail.com>
2012-12-03 14:56:40 +0530

    Added the shell script for installing the python

---
+++ installpython.sh
@@ -0,0 +1,10 @@
+VERSION="2.6.8"
+PYTHON="Python-$VERSION"
+MD5="c6e0420a21d8b23dee8b0195c9b9a125"
+PREFIX="`pwd`/usr"
+DOWNLOAD_PREFIX="http://www.python.org/ftp/python/$VERSION"
+DOWNLOADDIR="downloads"
+
+
+mkdir -p $DOWNLOADDIR
+
