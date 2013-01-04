4f8f6bb6eed5aabd3fc81e9e7414d7d1eaa85112
Sadashiv <sadamsrit@gmail.com>
2013-01-05 00:37:34 +0530

    cheking the python exist or not

--- installpython.sh
+++ installpython.sh
@@ -1,20 +1,23 @@
-VERSION="2.6.8"
+VERSION="2.6.6"
 PYTHON="Python-$VERSION"
-MD5="c6e0420a21d8b23dee8b0195c9b9a125"
+MD5="cf4e6881bb84a7ce6089e4a307f71f14"
 PREFIX="`pwd`/usr"
 DOWNLOAD_PREFIX="http://www.python.org/ftp/python/$VERSION"
 DOWNLOADDIR="downloads"
-
-
 mkdir -p $DOWNLOADDIR
 cd $DOWNLOADDIR
 wget -c $DOWNLOAD_PREFIX/$PYTHON.tar.bz2
+if [ -e "Python-2.6.8.tar.bz2" ]; then
+echo "Python exits"
+else
+wget -c $DOWNLOAD_PREFIX/$PYTHON.tar.bz2
+fi
 cd ..
 tar jxvf $DOWNLOADDIR/$PYTHON.tar.bz2
 cd $PYTHON
 ./configure --prefix=$PREFIX --enable-unicode=ucs4
-make
-make install
+make >  /dev/null
+make install  > /dev/null
 cd ..
 rm -fr $PYTHON
 
