7efb644928836fd2f0d780f09d02e6d236b72d15
Sadashiv <sadamsrit@gmail.com>
2013-03-29 19:58:55 +0530

    Added the file functionality

---
+++ python/fileinput.py
@@ -0,0 +1,25 @@
+import fileinput
+#fileinput — Iterate over lines from multiple input streams
+import stat
+#stat — Interpreting stat() results
+import statvfs
+#statvfs — Constants used with os.statvfs()
+import filecmp
+#filecmp — File and Directory Comparisons
+filecmp.cmp('','')
+import glob
+#glob — Unix style pathname pattern expansion
+import fnmatch
+#ifnmatch — Unix filename pattern matching
+import linecache
+#linecache — Random access to text lines
+shutil — High-level file operations
+The shutil module offers a number of high-level operations on files and collections of files.
+import shutil
+shutil.copy(src,dst)
+shutil.coptree(src,dst)
+shutil.rmtree(src,dst)
+shutil.rmtree(src,dst)
+dircache — Cached directory listings
+import dircache
+
