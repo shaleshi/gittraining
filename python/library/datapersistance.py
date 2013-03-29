4822a61b34a08ed73b80730f0a3c228cdcb02faa
Sadashiv <sadamsrit@gmail.com>
2013-03-30 02:47:43 +0530

    data

---
+++ python/library/datapersistance.py
@@ -0,0 +1,12 @@
+""""pickle — Python object serialization
+copy_reg — Register pickle support functions
+shelve — Python object persistence
+marshal — Internal Python object serialization
+anydbm — Generic access to DBM-style databases
+dbm — Simple “database” interface
+gdbm — GNU’s reinterpretation of dbm
+dbhash — DBM-style interface to the BSD database library
+bsddb — Interface to Berkeley DB library
+dumbdbm — Portable DBM implementation
+http://docs.python.org/2.6/library/sqlite3.html"""
+
