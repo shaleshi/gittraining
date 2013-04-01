b13645299bf63eb6e705eaf31cfaef4158788c25
Sadashiv <sadamsrit@gmail.com>
2013-04-01 23:24:03 +0530

    Added the optparse and logging

--- python/osinterfaces.py
+++ python/osinterfaces.py
@@ -15,11 +15,35 @@ import time
 import datetime
 #optparse  More powerful command line option parser
 from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
+def main():
+	usage 	= "usage:%prog [options] arg1 arg2"
+	parser = OptionParser(usage=usage)
+	parser.add_option("-f", "--file", dest="filename",
                   help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
+	parser.add_option("-q", "--quiet",
                   action="store_false", dest="verbose", default=True,
                   help="don't print status messages to stdout")
 
-(options, args) = parser.parse_args()
+	(options, args) = parser.parse_args()
+if __name__ == '__main__':
+	main()
+
+#parser.add_option(opt_str, ...,attr=value, ...) #basic syntax
+#Once all of your options are defined, instruct optparse to parse your programs command line
+#(options, args) = parser.parse_args()
+#parse_args takes two arguments
+#options, an object containing values for all of your options e.g. if "--file" takes a single string argument
+#args, the list of positional arguments leftover after parsing options
+#The most common option action is store, which tells optparse to take the next argument (or the remainder of the current argument),
+# ensure that it is of the correct type, and store it to your chosen destination.
+#getopt   Parser for command line options
+#logging   Logging facility for Python
+import logging
+LOG_FILENAME = 'log_file.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+logging.debug("This message should go into the log file")
+#getpass   Portable password input
+#curses   Terminal handling for character-cell displays
+#curses   ascii Utilities for ASCII characters
+#curses   panel A panel stack extension for curses
+#errno    Standard errno system symbols
