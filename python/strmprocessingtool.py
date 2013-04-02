815db3099ccbad3e9d717f45a79ed52da0185524
Sadashiv <sadamsrit@gmail.com>
2013-04-02 18:11:58 +0530

    Added the Structured Markup Processing Tool 'SMPL'

---
+++ python/strmprocessingtool.py
@@ -0,0 +1,10 @@
+#Structured Markup Processing Tools
+#htmllib  A parser for HTML documents
+#sgmllib  Simple SGML parser
+#xml.parsers.expat — Fast XML parsing using Expat
+#xml.dom  The Document Object Model API
+#xml.dom.minidom  Lightweight DOM implementation
+#xml.dom.pulldom  Support for building partial DOM trees
+#xml.sax  Support for SAX2 parsers
+#xml.sax.handler  Base classes for SAX handlers
+#xml.sax.saxutils  SAX Utilities
