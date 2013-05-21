dc568f45d2d38f80bc3168a034161ae18b1362cb
Sadashiv <sadamsrit@gmail.com>
2013-05-21 19:42:11 +0530

    Preparing release 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="folder",
-    version="1.0.0",
+    version='1.2.1',
     description="Folder creation for testing",
     author="Sadashiv",
     url="http://www.sadashivb.blogspot.com",
